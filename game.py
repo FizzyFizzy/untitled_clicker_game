30ab679d232b28ecea727df4ba3a5c7be9f9150a
FizzyFizzy <fizzyfizzyfizzyfizzy@gmail.com>
2026-01-01 22:03:51 +0100

    main function + get rid of video init error on close

--- game.py
+++ game.py
@@ -1,11 +1,17 @@
 import pygame
 
-pygame.init()
 
-WIDTH, HEIGHT = 1000, 1000
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+def main():
+    pygame.init()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    WIDTH, HEIGHT = 1000, 1000
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
