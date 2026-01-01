83526371c644f6aa3f9d9bc9f375f574a1899878
FizzyFizzy <fizzyfizzyfizzyfizzy@gmail.com>
2026-01-01 22:31:48 +0100

    screen name and icon setup

--- game.py
+++ game.py
@@ -3,9 +3,15 @@ import pygame
 
 def main():
     pygame.init()
+    icon = pygame.image.load("icon.png")
 
+    #screen setup
     WIDTH, HEIGHT = 1000, 1000
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Untitled Clicker Game")
+    pygame.display.set_icon(icon)
+
+    #main loop
     running = True
     while running:
         for event in pygame.event.get():
